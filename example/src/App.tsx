2290ea846e128a447edde0e1e4d6fe4e491638d1
jeancarrie-kima <jean@kima.finance>
2023-06-27 10:43:25 +0300

    fix dependencies

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
               colorMode: ColorModeOptions.light,
               fontSize: FontSizeOptions.medium
             }}
-            mode={ModeOptions.status}
+            mode={ModeOptions.payment}
             useFIAT={true}
             kimaBackendUrl='http://localhost:3001'
             kimaNodeProviderQuery='https://api_testnet.kima.finance'
@@ -40,11 +40,11 @@ const App = () => {
                 fontWeight: '500'
               }
             }}
-            helpURL='https://t.me/GoodDollarX'
+            // helpURL='https://t.me/GoodDollarX'
             // compliantOption={false}
             transactionOption={{
               targetChain: SupportNetworks.AVALANCHE,
-              targetAddress: '0x67cc400c434F691Ed45e452dC8F2Baf0101a9B63',
+              targetAddress: '0x8222ADB2A2092c3774105a5F558987265D920C09',
               amount: 5
             }}
             txId={-1}
