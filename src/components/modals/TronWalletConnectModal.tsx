8d47fbee492957bc811753b503e7bbadd0895509
kima-bastian <bastian.andres@kima.finance>
2024-11-08 14:08:09 -0300

    refactor: tron wallet connection

--- src/components/modals/TronWalletConnectModal.tsx
+++ src/components/modals/TronWalletConnectModal.tsx
@@ -1,66 +1,46 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { CrossIcon } from '../../assets/icons'
 import { setTronConnectModal } from '../../store/optionSlice'
-import {
-  selectTronConnectModal,
-  selectTronProvider,
-  selectTheme
-} from '../../store/selectors'
-
-import { useWallet } from '@tronweb3/tronwallet-adapter-react-hooks'
+import { selectTronConnectModal, selectTheme } from '../../store/selectors'
 import { TronWalletSelect } from '../reusable'
+import AccountDetailsModal from './AccountDetailsModal'
 
 const TronWalletConnectModal = () => {
   const dispatch = useDispatch()
   const theme = useSelector(selectTheme)
   const connectModal = useSelector(selectTronConnectModal)
-  const selectedProvider = useSelector(selectTronProvider)
-  const { select, connect } = useWallet()
-
-  useEffect(() => {
-    const handleConnect = async () => {
-      try {
-        select(selectedProvider)
-        await connect()
-        dispatch(setTronConnectModal(false))
-      } catch (e) {
-        console.log(e)
-      }
-    }
-
-    if (selectedProvider) {
-      handleConnect()
-    }
-  }, [selectedProvider])
 
   return (
-    <div
-      className={`kima-modal wallet-connect ${theme.colorMode} ${
-        connectModal ? 'open' : ''
-      }`}
-    >
-      <div className='modal-overlay' />
-      <div className={`modal-content-container ${theme.colorMode}`}>
-        <div className='kima-card-header'>
-          <div className='topbar'>
-            <div className='title'>
-              <h3>Connect Wallet</h3>
-            </div>
-            <div className='control-buttons'>
-              <button
-                className='icon-button'
-                onClick={() => dispatch(setTronConnectModal(false))}
-              >
-                <CrossIcon
-                  fill={theme.colorMode === 'light' ? 'black' : 'white'}
-                />
-              </button>
+    <div>
+      <AccountDetailsModal />
+      <div
+        className={`kima-modal wallet-connect ${theme.colorMode} ${
+          connectModal ? 'open' : ''
+        }`}
+      >
+        <div className='modal-overlay' />
+        <div className={`modal-content-container ${theme.colorMode}`}>
+          <div className='kima-card-header'>
+            <div className='topbar'>
+              <div className='title'>
+                <h3>Connect Wallet</h3>
+              </div>
+              <div className='control-buttons'>
+                <button
+                  className='icon-button'
+                  onClick={() => dispatch(setTronConnectModal(false))}
+                >
+                  <CrossIcon
+                    fill={theme.colorMode === 'light' ? 'black' : 'white'}
+                  />
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className='modal-content'>
-          <TronWalletSelect />
+          <div className='modal-content'>
+            <TronWalletSelect />
+          </div>
         </div>
       </div>
     </div>
