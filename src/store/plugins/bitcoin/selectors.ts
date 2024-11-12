82a52b80ca291589527dce9d1210bd383251a4e3
HugoG-Crypto <hugo.gonzalez@kima.finance>
2024-11-12 10:30:37 -0800

    feat(fiat): Add expire time selector to fiat store

--- src/store/plugins/bitcoin/selectors.ts
+++ src/store/plugins/bitcoin/selectors.ts
@@ -3,9 +3,10 @@
 import { RootState } from '../../index'
 
 /*
- * Bitconi Plugin Selectors
+ * Bitcoin Plugin Selectors
  */
 
 export const selectBitcoinAddress = (state: RootState) => state.bitcoin.bitcoinAddress
 export const selectBitcoinPubkey = (state: RootState) => state.bitcoin.bitcoinPubkey
 export const selectPendingTxPopup = (state: RootState) => state.core.pendingTxPopup
+export const selectExpireTime = (state: RootState) => state.fiat.expireTime
