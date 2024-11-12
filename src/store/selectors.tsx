bc389b680171e48c1fb480108417898b93092f6b
HugoG-Crypto <hugo.gonzalez@kima.finance>
2024-11-11 21:21:07 -0800

    Add and rearrange labels

--- src/store/selectors.tsx
+++ src/store/selectors.tsx
@@ -4,135 +4,161 @@ import { RootState } from '.'
  * Option
  */
 
-//Core
+// Core
 export const selectNetworkOption = (state: RootState) =>
   state.option.networkOption
-//Core
+// Core
 export const selectTokenOptions = (state: RootState) =>
   state.option.tokenOptions
 
-//Core
+// Core
 export const selectTheme = (state: RootState) => state.option.theme
 
-//Core
+// Core
 export const selectKimaExplorer = (state: RootState) =>
   state.option.kimaExplorerUrl
 
-//Core
+// Core
 export const selectSourceChain = (state: RootState) => state.option.sourceChain
 
-//Core
+// Core
 export const selectTargetChain = (state: RootState) => state.option.targetChain
 
-//Core
+// Core
 export const selectTargetAddress = (state: RootState) =>
   state.option.targetAddress
+
+// Bitcoin
 export const selectBitcoinAddress = (state: RootState) =>
   state.option.bitcoinAddress
+
+// Bitcoin
 export const selectBitcoinPubkey = (state: RootState) =>
   state.option.bitcoinPubkey
 
-//Solana
+// Solana
 export const selectSolanaConnectModal = (state: RootState) =>
   state.option.solanaConnectModal
 
-//Tron
+// Tron
 export const selectTronConnectModal = (state: RootState) =>
   state.option.tronConnectModal
-export const selectAccountDetailsModal = (state: RootState) =>
-  state.option.accountDetailsModal
 
-//Core
+// Core
 export const selectHelpPopup = (state: RootState) => state.option.helpPopup
 
-//Core
+// Core
 export const selectHashPopup = (state: RootState) => state.option.hashPopup
 
-//Core
+// Core
 export const selectPendingTxPopup = (state: RootState) =>
   state.option.pendingTxPopup
 export const selectBankPopup = (state: RootState) => state.option.bankPopup
 
-//Solana
+// Solana
 export const selectSolanaProvider = (state: RootState) =>
   state.option.solanaProvider
 
-//Tron
+// Tron
 export const selectTronProvider = (state: RootState) =>
   state.option.tronProvider
 export const selectProvider = (state: RootState) => state.option.provider
 
-//Core
+// Core
 export const selectDappOption = (state: RootState) => state.option.dAppOption
 
-//Core | Wallet
+// Core | Wallet
 export const selectWalletAutoConnect = (state: RootState) =>
   state.option.walletAutoConnect
 
-//Core
+// Core
 export const selectSubmitted = (state: RootState) => state.option.submitted
 
-//Core
+// Core
 export const selectTransactionOption = (state: RootState) =>
   state.option.transactionOption
 
-//Core
+// Core
 export const selectAmount = (state: RootState) => state.option.amount
 
-//Core
+// Core
 export const selectErrorHandler = (state: RootState) =>
   state.option.errorHandler
 
-//Core | Wallet
+// Core | Wallet
 export const selectKeplrHandler = (state: RootState) =>
   state.option.keplrHandler
 
-//Core
+// Core
 export const selectCloseHandler = (state: RootState) =>
   state.option.closeHandler
 
-//Core
+// Core
 export const selectSuccessHandler = (state: RootState) =>
   state.option.successHandler
 
-//Core
+// Core
 export const selectSwitchChainHandler = (state: RootState) =>
   state.option.switchChainHandler
 
-//Core
+// Core
 export const selectInitChainFromProvider = (state: RootState) =>
   state.option.initChainFromProvider
 
-//Core
+// Core
 export const selectServiceFee = (state: RootState) => state.option.serviceFee
 
-//Core
+// Core
 export const selectMode = (state: RootState) => state.option.mode
-export const selectFeeDeduct = (state: RootState) => state.option.feeDeduct
-export const selectBackendUrl = (state: RootState) => state.option.backendUrl
-export const selectNodeProviderQuery = (state: RootState) =>
-  state.option.nodeProviderQuery
-export const selectGraphqlProviderQuery = (state: RootState) =>
-  state.option.graphqlProviderQuery
-export const selectTxId = (state: RootState) => state.option.txId
+
+// Core
 export const selectSourceCurrency = (state: RootState) =>
   state.option.sourceCurrency
+
+// Core
 export const selectTargetCurrency = (state: RootState) =>
   state.option.targetCurrency
+
+// Core | Should be move to Fiat
 export const selectCompliantOption = (state: RootState) =>
   state.option.compliantOption
+
+// Core | Should be move to Fiat
 export const selectSourceCompliant = (state: RootState) =>
   state.option.sourceCompliant
+// Core | Should be move to Fiat
 export const selectTargetCompliant = (state: RootState) =>
   state.option.targetCompliant
-export const selectUseFIAT = (state: RootState) => state.option.useFIAT
-export const selectBankDetails = (state: RootState) => state.option.bankDetails
+
+// Core
+export const selectBackendUrl = (state: RootState) => state.option.backendUrl
+
+// Core
+export const selectFeeDeduct = (state: RootState) => state.option.feeDeduct
+
+// Core
+export const selectNodeProviderQuery = (state: RootState) =>
+  state.option.nodeProviderQuery
+
+// Core
+export const selectGraphqlProviderQuery = (state: RootState) =>
+  state.option.graphqlProviderQuery
+
+// Core
 export const selectTargetChainFetching = (state: RootState) =>
   state.option.targetNetworkFetching
+
+// Core
+export const selectTxId = (state: RootState) => state.option.txId
+
+//Unmarked:
+
+export const selectAccountDetailsModal = (state: RootState) =>
+  state.option.accountDetailsModal
+export const selectUseFIAT = (state: RootState) => state.option.useFIAT
+export const selectBankDetails = (state: RootState) => state.option.bankDetails
+
 export const selectSignature = (state: RootState) => state.option.signature
 export const selectUuid = (state: RootState) => state.option.uuid
 export const selectKycStatus = (state: RootState) => state.option.kycStatus
 export const selectExpireTime = (state: RootState) => state.option.expireTime
-export const selectPendingTxs = (state: RootState) => state.option.pendingTxs
-export const selectPendingTxData = (state: RootState) =>
-  state.option.pendingTxData
