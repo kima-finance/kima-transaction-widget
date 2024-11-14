187116c7992b96ba91573f1b4d64c15a8ac8a276
HugoG-Crypto <hugo.gonzalez@kima.finance>
2024-11-14 09:15:54 -0800

    chore: add networks config for multiple chains  This commit adds network configurations for multiple chains, including Ethereum, BNB Smart Chain, Polygon, Arbitrum, Optimism, Avalanche, and zkEVM. These configurations are used to connect to the respective chain's RPC endpoints, explorers, and other relevant information.

---
+++ plugins/evm/config/networks.ts
@@ -0,0 +1,113 @@
+// plugins/evm/config/networks.ts
+
+const ethereumSepolia = {
+  chainId: 11155111,
+  name: 'Ethereum Sepolia',
+  currency: 'ETH',
+  explorerUrl: 'https://sepolia.etherscan.io',
+  rpcUrl: 'https://ethereum-sepolia-rpc.publicnode.com'
+}
+
+const ethereum = {
+  chainId: 1,
+  name: 'Ethereum Mainnet',
+  currency: 'ETH',
+  explorerUrl: 'https://etherscan.io',
+  rpcUrl: 'https://endpoints.omniatech.io/v1/eth/mainnet/public'
+}
+
+const bscTestnet = {
+  chainId: 97,
+  name: 'BNB Smart Chain Testnet',
+  currency: 'tBNB',
+  explorerUrl: 'https://testnet.bscscan.com',
+  rpcUrl: 'https://endpoints.omniatech.io/v1/bsc/testnet/public'
+}
+
+const bsc = {
+  chainId: 56,
+  name: 'BNB Smart Chain Mainnet',
+  currency: 'BNB',
+  explorerUrl: 'https://bscscan.com',
+  rpcUrl: 'https://bsc-dataseed.binance.org/'
+}
+
+const polygonAmoy = {
+  chainId: 80002,
+  name: 'Amoy',
+  currency: 'MATIC',
+  explorerUrl: 'https://www.oklink.com/amoy',
+  rpcUrl: 'https://rpc-amoy.polygon.technology'
+}
+
+const polygon = {
+  chainId: 137,
+  name: 'Polygon Mainnet',
+  currency: 'MATIC',
+  explorerUrl: 'https://polygonscan.com',
+  rpcUrl: 'https://polygon-rpc.com/'
+}
+
+const arbitrumSepolia = {
+  chainId: 421614,
+  name: 'Arbitrum Sepolia Testnet',
+  currency: 'ETH',
+  explorerUrl: 'https://sepolia.arbiscan.io/',
+  rpcUrl: 'https://sepolia-rollup.arbitrum.io/rpc'
+}
+
+const arbitrum = {
+  chainId: 42161,
+  name: 'Arbitrum Mainnet',
+  currency: 'ETH',
+  explorerUrl: 'https://arbiscan.io',
+  rpcUrl: 'https://arb1.arbitrum.io/rpc'
+}
+
+const optimismSepola = {
+  chainId: 11155420,
+  name: 'OP Sepolia',
+  currency: 'ETH',
+  explorerUrl: 'https://sepolia-optimism.etherscan.io',
+  rpcUrl: 'https://sepolia.optimism.io'
+}
+
+const optimism = {
+  chainId: 10,
+  name: 'OP Mainnet',
+  currency: 'ETH',
+  explorerUrl: 'https://optimistic.etherscan.io',
+  rpcUrl: 'https://mainnet.optimism.io'
+}
+
+const avalancheFuji = {
+  chainId: 43113,
+  name: 'Avalanche Fuji Testnet',
+  currency: 'AVAX',
+  explorerUrl: 'https://testnet.snowtrace.io',
+  rpcUrl: 'https://api.avax-test.network/ext/bc/C/rpc'
+}
+
+const avalanche = {
+  chainId: 43114,
+  name: 'Avalanche Mainnet',
+  currency: 'AVAX',
+  explorerUrl: 'https://snowtrace.io',
+  rpcUrl: 'https://api.avax.network/ext/bc/C/rpc'
+}
+
+const zkEVMTestnet = {
+  chainId: 2442,
+  name: 'Polygon zkEVM Cardona Testnet',
+  currency: 'ETH',
+  explorerUrl: 'https://cardona-zkevm.polygonscan.com',
+  rpcUrl: 'https://polygon-zkevm-cardona.blockpi.network/v1/rpc/public'
+}
+
+const zkEVM = {
+  chainId: 1101,
+  name: 'Polygon zkEVM',
+  currency: 'ETH',
+  explorerUrl: 'https://zkevm.polygonscan.com',
+  rpcUrl: 'https://zkevm-rpc.com'
+}
