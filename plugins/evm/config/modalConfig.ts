b8a5787a5527e8641517a6a97d85edfeb5066f55
HugoG-Crypto <hugo.gonzalez@kima.finance>
2024-11-14 09:25:43 -0800

    feat: pass network option to modal to determine available chains

--- plugins/evm/config/modalConfig.ts
+++ plugins/evm/config/modalConfig.ts
@@ -1,6 +1,7 @@
 // plugins/evm/config/modalConfig.ts
 
 import { createWeb3Modal, defaultConfig } from '@web3modal/ethers5/react'
+import { mainnetChains, testnetChains } from './networks'
 
 const metadata = {
   name: 'Kima Transaction Widget',
@@ -9,10 +10,13 @@ const metadata = {
   icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
 
-export const setupWeb3Modal = (walletConnectProjectId) =>
-  createWeb3Modal({
+export const setupWeb3Modal = (walletConnectProjectId, networkOption) => {
+  const chains = networkOption === 'mainnet' ? mainnetChains : testnetChains
+
+  return createWeb3Modal({
     ethersConfig: defaultConfig({ metadata }),
-    projectId: walletConnectProjectId || 'e579511a495b5c312b572b036e60555a',
+    chains,
+    projectId: walletConnectProjectId || 'e579511a495b5c312b572b036e60555a', // Default project ID
     enableAnalytics: false,
     featuredWalletIds: [
       'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
@@ -20,3 +24,4 @@ export const setupWeb3Modal = (walletConnectProjectId) =>
       '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0'
     ]
   })
+}
