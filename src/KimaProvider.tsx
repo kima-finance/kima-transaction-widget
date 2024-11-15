dfef4cea4d99c9134e18eac1233957e431f6cd43
HugoG-Crypto <hugo.gonzalez@kima.finance>
2024-11-14 16:38:50 -0800

    Pass networkOption to the plugin's walletProvider

--- src/KimaProvider.tsx
+++ src/KimaProvider.tsx
@@ -1,43 +1,49 @@
 // src/KimaProvider.tsx
-import React, { ReactNode, useEffect } from 'react';
-import { Provider } from 'react-redux';
-import { useSelector } from 'react-redux';
-import { store } from './store';
-import { selectAllPlugins } from './store/pluginSlice';
+import React, { ReactNode, useEffect } from 'react'
+import { Provider } from 'react-redux'
+import { useSelector } from 'react-redux'
+import { store } from './store'
+import { selectAllPlugins } from './store/pluginSlice'
 
 interface KimaProviderProps {
-  walletConnectProjectId: string;
-  networkOption: string;  // Define types based on NetworkOptions
-  children: ReactNode;
+  walletConnectProjectId: string
+  networkOption: string // Define types based on NetworkOptions
+  children: ReactNode
 }
 
-const KimaProvider = ({ walletConnectProjectId, networkOption, children }: KimaProviderProps) => {
-  const plugins = useSelector(selectAllPlugins);
+const KimaProvider = ({
+  walletConnectProjectId,
+  networkOption,
+  children
+}: KimaProviderProps) => {
+  const plugins = useSelector(selectAllPlugins)
 
-  // Initialize EVM plugin if loaded
+  // Initialize plugins with walletConnectProjectId and networkOption
   useEffect(() => {
-    const evmPlugin = plugins.find((plugin) => plugin.id === 'evm');
-    if (evmPlugin && evmPlugin.initialize) {
-      evmPlugin.initialize(walletConnectProjectId);
-    }
-  }, [walletConnectProjectId, plugins]);
+    plugins.forEach((plugin) => {
+      if (plugin.initialize) {
+        plugin.initialize(walletConnectProjectId, networkOption)
+      }
+    })
+  }, [walletConnectProjectId, networkOption, plugins])
 
-  // Dynamically wrap children with each registered plugin provider
-  const WrappedProviders = plugins.reduce((Wrapped, plugin) => {
-    return ({ children }) => (
-      <plugin.provider>
-        <Wrapped>{children}</Wrapped>
-      </plugin.provider>
-    );
-  }, ({ children }) => <>{children}</>);
+  // Dynamically wrap children with each registered plugin provider, passing networkOption
+  const WrappedProviders = plugins.reduce(
+    (Wrapped, plugin) => {
+      return ({ children }) => (
+        <plugin.provider networkOption={networkOption}>
+          <Wrapped>{children}</Wrapped>
+        </plugin.provider>
+      )
+    },
+    ({ children }) => <>{children}</>
+  )
 
   return (
     <Provider store={store}>
-      <WrappedProviders>
-        {children}
-      </WrappedProviders>
+      <WrappedProviders>{children}</WrappedProviders>
     </Provider>
-  );
-};
+  )
+}
 
-export default KimaProvider;
+export default KimaProvider
