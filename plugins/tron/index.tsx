99a9fcc95d83d5fbeda54a98745654811fc536c8
HugoG-Crypto <hugo.gonzalez@kima.finance>
2024-11-20 02:08:46 -0800

    chore: Add typing for Tron Plugin provider props

--- plugins/tron/index.tsx
+++ plugins/tron/index.tsx
@@ -1,11 +1,18 @@
 // plugins/tron/index.tsx
+import React, { ReactNode } from 'react' // Explicitly import React
 import store from '@store/store'
 import { registerPlugin } from '@store/pluginSlice'
 import WalletProvider from '@plugins/tron/features/walletConnect/WalletProvider'
 
+// Define the type for the provider props
+interface ProviderProps {
+  children: ReactNode
+  networkOption: 'testnet' | 'mainnet'
+}
+
 const TronPlugin = {
   id: 'tron',
-  provider: ({ children, networkOption }) => (
+  provider: ({ children, networkOption }: ProviderProps) => (
     <WalletProvider networkOption={networkOption}>{children}</WalletProvider>
   )
 }
