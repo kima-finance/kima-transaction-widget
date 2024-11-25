38c39fdd71dbfd571bdfa4fba8ee2e34fe60ab24
HugoG-Crypto <hugo.gonzalez@kima.finance>
2024-11-25 01:37:15 -0800

    feat(utils): add getChainData function

---
+++ plugins/evm/utils/getChainData.ts
@@ -0,0 +1,19 @@
+async function getChainData() {
+  const response = await fetch('http://localhost:3001/chains/chain', {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json'
+    }
+  })
+  const data = await response.json()
+  const chainData = data.Chain ?? []
+  const chains = chainData
+    .filter((chain) => !chain.disabled && chain.isEvm)
+    .map((chain) => {
+      const { name, symbol, tokens } = chain
+      return { name, symbol, tokens }
+    })
+  return chains
+}
+
+export default getChainData
