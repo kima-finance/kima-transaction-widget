5bd4c01b4294c2c943d6e1c34750b4d8a942eca2
mayjer-kima <jeremy.may@kima.finance>
2024-12-03 15:24:40 -0800

    fix: pool export

--- src/hooks/useGetPools.tsx
+++ src/hooks/useGetPools.tsx
@@ -1,11 +1,11 @@
 import { NetworkOptions } from '@interface'
 import { useQuery } from '@tanstack/react-query'
-import { getPools } from 'src/services/poolsApi'
+import { getPoolsBalances } from 'src/services/poolsApi'
 
 const useGetPools = (backendUrl: string, networkOption: NetworkOptions) => {
   const { data, error, isLoading } = useQuery<Array<any>>({
     queryKey: ['pools', networkOption],
-    queryFn: async () => await getPools(backendUrl),
+    queryFn: async () => await getPoolsBalances(backendUrl),
     refetchInterval: 300000,
     enabled: !!backendUrl && !!networkOption
   })
