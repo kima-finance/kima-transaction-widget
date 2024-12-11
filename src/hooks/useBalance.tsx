b28abad16ec822d58d95999571de960d452faaa3
HugoG-Crypto <hugo.gonzalez@kima.finance>
2024-12-11 11:28:58 -0800

    Fix(useBalance.tsx): Ensure the correct value is returned when no current plugin ID exists

--- src/hooks/useBalance.tsx
+++ src/hooks/useBalance.tsx
@@ -1,33 +1,25 @@
 import { getAllPlugins } from '@pluginRegistry'
 import useGetCurrentPlugin from './useGetCurrentPlugin'
-
 // Preload all plugins
 const allPlugins = getAllPlugins()
-
-export default function useBalance() {
+export default function useBalance(): number {
   // Get the current plugin and extract its ID
   const { currentPlugin } = useGetCurrentPlugin()
   const currentPluginID = currentPlugin?.data?.id
-
   // Call useBalance for every plugin in a stable order
   const pluginEntries = Object.entries(allPlugins)
   const allBalances = pluginEntries.map(([pluginID, plugin]) => {
     const balanceData = plugin.useBalance()
     return { pluginID, ...balanceData }
   })
-  console.info('cBalances: ', allBalances);
-  console.info('cBalance ID:', currentPluginID)
-
   // If we have a current plugin ID, filter down to just that plugin's balance
   // Otherwise, return all (in case currentPluginID is not defined)
   if (currentPluginID) {
     const mainBalance = allBalances.filter(
       ({ pluginID }) => pluginID === currentPluginID
     )
-    const balance = mainBalance[0]?.balance ?? 0;
-    console.info('cBalanceUpdated:', balance);
+    const balance = mainBalance[0]?.balance ?? 0
     return balance
   }
-
   return 0
 }
