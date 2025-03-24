28291a7886def7836c946925ff8d846c000992ca
kima-bastian <bastian.andres@kima.finance>
2025-03-24 00:11:59 -0300

    build: chaingpt 1st iteration fixes

--- dist/index.cjs
+++ dist/index.cjs
@@ -211,19 +211,16 @@ var Arrow = ({ width = 22, height = 25, fill = "none", ...rest }) => {
     "svg",
     {
       xmlns: "http://www.w3.org/2000/svg",
-      width,
-      height,
-      viewBox: "0 0 22 25",
-      fill,
-      ...rest
+      width: "16",
+      height: "16",
+      viewBox: "0 0 16 16",
+      fill: "none"
     },
-    /* @__PURE__ */ import_react7.default.createElement("path", { d: "M10.9974 0L10.9974 16.625", stroke: "#778DA3", strokeWidth: "2" }),
     /* @__PURE__ */ import_react7.default.createElement(
       "path",
       {
-        d: "M21.1249 14.2734L16.8822 18.5161C13.758 21.6403 8.69272 21.6403 5.56853 18.5161L1.32589 14.2734",
-        stroke: "#778DA3",
-        strokeWidth: "2"
+        d: "M3.3335 5.8339C3.37107 5.75069 3.39704 5.65895 3.4478 5.58548C3.5745 5.40323 3.75635 5.33301 3.97577 5.33301C6.58444 5.33504 9.1927 5.33423 11.8014 5.33423C11.8865 5.33423 11.9712 5.33382 12.0564 5.33423C12.3209 5.33504 12.5136 5.46168 12.6167 5.7032C12.717 5.93781 12.6627 6.15579 12.4904 6.34251C12.4716 6.3628 12.4524 6.38229 12.4328 6.40177C11.1103 7.74493 9.78781 9.08889 8.4633 10.4304C8.38936 10.5051 8.30343 10.579 8.20911 10.6192C8.00327 10.7064 7.80424 10.6715 7.63038 10.5234C7.59321 10.4917 7.55804 10.4572 7.52367 10.4227C6.21993 9.09863 4.917 7.77456 3.61287 6.4521C3.48937 6.32668 3.37306 6.19922 3.3335 6.019C3.3335 5.9573 3.3335 5.8956 3.3335 5.8339Z",
+        fill: "white"
       }
     )
   );
@@ -998,7 +995,7 @@ var ExternalUrl_default = ExternalUrl;
 
 // src/assets/icons/Base.tsx
 var import_react33 = __toESM(require("react"), 1);
-var Base = ({ width = 27, height = 27, ...rest }) => {
+var Base = ({ width = 30, height = 30, ...rest }) => {
   return /* @__PURE__ */ import_react33.default.createElement(
     "svg",
     {
@@ -5219,20 +5216,17 @@ var TransactionWidget = ({ theme }) => {
           position: "absolute"
         },
         toastOptions: {
-          duration: 5 * 1e3,
+          duration: 3 * 1e3,
           style: {
             fontFamily: "Manrope",
             position: "relative",
-            top: "3rem",
-            right: "1.5rem",
             margin: "5px 0",
-            padding: ".7rem 1.5rem",
-            color: theme.colorMode === "light" /* light */ ? "black" : "white",
-            fontSize: "1em",
-            borderRadius: "50px",
-            border: "1px solid #B900004D",
-            background: theme.colorMode === "light" /* light */ ? "#F7F8F9" : "#242732",
-            fontWeight: "bolder"
+            padding: "8px",
+            color: "white",
+            fontSize: "16px",
+            borderRadius: "2px",
+            border: "1px solid #353539",
+            background: "#111115"
           }
         }
       }
@@ -6298,6 +6292,8 @@ var TransferWidget = ({
       setFormStep(0);
     }
     if (formStep === 0) {
+      dispatch(setTargetAddress(""));
+      dispatch(setAmount(""));
       closeHandler && closeHandler(0);
     }
   };
@@ -6463,12 +6459,12 @@ var TransferWidget = ({
             top: windowWidth > 768 ? "3rem" : "1.5rem",
             right: windowWidth > 768 ? "1.5rem" : "0rem",
             margin: "5px 0",
-            padding: ".7rem 1.5rem",
-            color: theme.colorMode === "light" /* light */ ? "black" : "white",
-            fontSize: "1em",
-            borderRadius: "50px",
-            border: "1px solid #B900004D",
-            background: theme.colorMode === "light" /* light */ ? "#F7F8F9" : "#242732"
+            padding: "8px",
+            color: "white",
+            fontSize: "16px",
+            borderRadius: "2px",
+            border: "1px solid #353539",
+            background: "#111115"
           }
         }
       }
