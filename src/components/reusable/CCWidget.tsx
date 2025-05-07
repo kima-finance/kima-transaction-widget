0f8072ea3bd5f95ab35847bc8bdce03637a1ba4d
kima-bastian <bastian.andres@kima.finance>
2025-05-07 16:58:44 -0400

    refactor(cc-widget): iframe rendering vars

--- src/components/reusable/CCWidget.tsx
+++ src/components/reusable/CCWidget.tsx
@@ -4,6 +4,7 @@ import {
   selectBackendUrl,
   selectCCTransactionStatus,
   selectFeeDeduct,
+  selectNetworkOption,
   selectServiceFee
 } from '@store/selectors'
 import { Loading180Ring } from '@assets/loading'
@@ -12,12 +13,14 @@ import { setCCTransactionId, setCCTransactionStatus } from '@store/optionSlice'
 import { formatBigInt } from 'src/helpers/functions'
 import { v4 as uuidv4 } from 'uuid'
 import { useCCTransactionId } from '../../hooks/useCCTransactionId'
+import { NetworkOptions } from '@interface'
 
 const CCWidget = () => {
   const dispatch = useDispatch()
   const feeDeduct = useSelector(selectFeeDeduct)
   const backendUrl = useSelector(selectBackendUrl)
   const ccTransactionStatus = useSelector(selectCCTransactionStatus)
+  const networkOption = useSelector(selectNetworkOption)
 
   const { transactionValues } = useSelector(selectServiceFee)
   const randomUserIdRef = useRef(uuidv4())
@@ -45,13 +48,30 @@ const CCWidget = () => {
   const [isLoading, setIsLoading] = useState(true)
   const { width, updateWidth } = useWidth()
 
+  // IMPORTANT: for staging use the same as mainnet
+  const baseUrl = useMemo(
+    () =>
+      `https://widget${networkOption === NetworkOptions.testnet ? '-sandbox':''}.depasify.com`,
+    [networkOption]
+  )
+
+  // IMPORTANT: for staging use KimaStage
+  const partnerId = useMemo(
+    () => `Kima${networkOption === NetworkOptions.testnet && 'Test'}`,
+    [networkOption]
+  )
+
   useEffect(() => {
     if (width === 0) updateWidth(window.innerWidth)
   }, [width, updateWidth])
 
   useEffect(() => {
     const handleMessage = (event: MessageEvent) => {
-      if (event.origin !== 'https://widget.depasify.com') return
+      if (event.origin !== baseUrl) {
+        // console.log("event origin missmatch: ", event.origin)
+        // console.log("event: ", event)
+        return
+      }
       console.log('postMessage: new message: ', event)
       if (event.data.type === 'isCompleted') {
         // set the transaction to success
@@ -91,7 +111,7 @@ const CCWidget = () => {
             ? 0
             : iframeHeight
         }
-        src={`https://widget.depasify.com/widgets/kyc?partner=KimaStage&user_uuid=${randomUserIdRef.current}&scenario=direct_card_payment&amount=${allowanceAmount}&currency=USD&trx_uuid=${data?.transactionId}&postmessage=true`}
+        src={`${baseUrl}/widgets/kyc?partner=${partnerId}&user_uuid=${randomUserIdRef.current}&scenario=direct_card_payment&amount=${allowanceAmount}&currency=USD&trx_uuid=${data?.transactionId}&postmessage=true`}
         loading='lazy'
         title='Credit Card Widget'
         onLoad={() => setIsLoading(false)}
