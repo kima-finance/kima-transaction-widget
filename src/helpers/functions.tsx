77e2c9516fe7d64061a3d602d78c166fa195df42
mayjer-kima <jeremy.may@kima.finance>
2025-05-20 14:50:46 -0700

    fix: bigint from number rounding issue

--- src/helpers/functions.tsx
+++ src/helpers/functions.tsx
@@ -52,7 +52,9 @@ export const toBigintAmount = (
   data: BigintAmount<string>
 ): BigintAmount<bigint> => {
   return {
-    value: BigInt(data.value),
+    // bigint values constructed from numbers can have rounding errors!
+    // so need to convert to string and then to bigint
+    value: BigInt(data.value.toString()),
     decimals: data.decimals
   }
 }
